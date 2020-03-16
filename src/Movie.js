20ee2dfb229f36f8999be5a7e64afb9363ad6192
cdis001 <shopingid@naver.com>
2020-03-16 11:20:26 +0900

    movie web init

---
+++ src/Movie.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import './Movie.css'
+
+const Movie = ({ id, year, title, summary, poster, genres }) => {
+  return (
+    <div className="movie">
+      <img className="movie__image" src={poster} alt={title} title={title} />
+      <div className="movie__data">
+        <h3 className="movie__title" title={title}>
+          {title}
+        </h3>
+        <h5 className="movie__year">{year}</h5>
+        <ul className="movie__genres">
+          {genres.map((genre, index) => (
+            <li className="genres__genre" key={index}>
+              {genre}
+            </li>
+          ))}
+        </ul>
+        <p className="movie__summary">{summary}</p>
+      </div>
+    </div>
+  )
+}
+
+Movie.propTypes = {
+  id: PropTypes.number.isRequired,
+  year: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired
+}
+
+export default Movie
